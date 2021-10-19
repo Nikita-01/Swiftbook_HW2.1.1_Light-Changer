aa33c502b3fb96ffcc03de9f4b22a0756cde3dc5
Nikita Ryzhkin <timongo@bk.ru>
2021-10-20 02:40:20 +0400

    Interface and algorithm creation

--- Light Changer/ViewController.swift
+++ Light Changer/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var changeLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        redLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        yellowLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        greenLightView.alpha = 0.3
+        changeLightButton.layer.cornerRadius = 0.2 * changeLightButton.frame.height
     }
-
-
+    var lightColor = "red"
+    @IBAction func changeLightButtonPressed() {
+        changeLightButton.setTitle("NEXT", for: .normal)
+        
+        switch lightColor {
+        case "red":
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            lightColor = "yellow"
+        case "yellow":
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            lightColor = "green"
+        case "green":
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            lightColor = "red"
+        default: break
+    }
 }
-
+}
